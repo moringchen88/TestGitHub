17a70e98c6455733fe97038e0aae2d429e669ad1
caic <christ-cai@163.com>
2020-10-19 15:22:44 +0800

    Initial Commit

---
+++ TestGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGitHub
+//
+//  Created by caichen on 2020/10/19.
+//  Copyright © 2020 caichen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
